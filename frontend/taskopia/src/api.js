6a9080afc89a8e963b120129040617f1d1a84ea8
сарофанов леша <billyroll03@gmail.com>
2024-07-17 16:53:26 +0300

    some fix

--- frontend/taskopia/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://localhost:7102', // Укажите URL вашего API
-  withCredentials: true, // Включает отправку и получение cookies
-});
-
-export default api;
--- frontend/taskopia/src/pages/Login.jsx
+++ frontend/taskopia/src/pages/Login.jsx
@@ -1,43 +1,98 @@
 import React, { useState } from 'react';
-import { login } from '../auth';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Heading,
+  useColorModeValue,
+  useToast,
+  Text,
+  Link
+} from '@chakra-ui/react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export default function Login() {
+  const toast = useToast();
+  const navigate = useNavigate();
+  
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
-      await login(email, password);
+      const response = await axios.post(`https://localhost:7102/Authentication/Login`, formData, { withCredentials: true });
+      console.log('User logged in:', response.data);
+      toast({
+        position: "top-left",
+        status: "success",
+        duration: 5000,
+        description: "Вход выполнен успешно!"
+      });
+      navigate('/');  // Перенаправление на домашнюю страницу при успешном логине
     } catch (error) {
-      console.error('Login failed', error);
+      console.error('Error logging user:', error.response.data);
+      toast({
+        position: "top-left",
+        status: "error",
+        duration: 5000,
+        description: "Ошибка при входе!"
+      });
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
+    <Box
+      minH="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box
+        bg={useColorModeValue('white', 'gray.700')}
+        p={8}
+        rounded="lg"
+        shadow="lg"
+        width="full"
+        maxW="md"
+      >
+        <Heading mb={6} textAlign="center">
+          Вход
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" value={formData.email} onChange={handleChange} />
+            </FormControl>
 
-export default Login;
+            <FormControl id="password" isRequired>
+              <FormLabel>Пароль</FormLabel>
+              <Input type="password" value={formData.password} onChange={handleChange} />
+            </FormControl>
+
+            <Button mt={4} size="lg" type="submit">
+              Войти
+            </Button>
+            <Text textAlign="center">
+              Еще нет аккаунта? <Link as={RouterLink} to="/register" color="teal.500">Зарегистрироваться</Link>
+            </Text>
+          </Stack>
+        </form>
+      </Box>
+    </Box>
+  );
+}
